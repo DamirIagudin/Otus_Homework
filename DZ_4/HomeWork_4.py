b23dd5be15ed8b66105c5719325fbba8f9edf39d
Damir Iagudin <damirjud@gmail.com>
2022-08-29 22:34:16 +0300

    DZ_4_zigzag

--- DZ_4/HomeWork_4.py
+++ DZ_4/HomeWork_4.py
@@ -1,5 +1,4 @@
 # Домашнее задание №4 к уроку №10 "Моделирование поведения IoT-устройства"
-
 import sys  # system params and function
 import paho.mqtt.client as mqtt  # mqtt paho
 import json  # json converter
@@ -9,41 +8,54 @@ from datetime import datetime  # for date
 
 ### machine and logic
 class Machine():
-    def __init__(self, mqtt_client, details=0, progress=0, temperature=21, light=True):
+    def __init__(self, mqtt_client, progress, temperature=21, light=True):
         self.mqtt_client = mqtt_client
         self.topic_data = "data/state"
         self.topic_change_temp = "command/change_temp"
         self.topic_change_light = "command/change_light"
         self.temperatureValue = temperature
         self.lightValue = light
-        self.detail_count = details
         self.progress = progress
+        self.course = 'forward_X'
 
         self.mqtt_client.subscribe(self.topic_change_temp)
-        self.mqtt_client.message_callback_add(self.topic_change_temp, self.command_change_temp)
+        self.mqtt_client.message_callback_add(self.topic_change_temp, self.command_change_temperature)
+
         self.mqtt_client.subscribe(self.topic_change_light)
         self.mqtt_client.message_callback_add(self.topic_change_light, self.command_change_light)
 
-    def command_change_temp(self, client, userdata, message):
-        print("CMD: Change temp, value: %s" % message.payload)
+    def command_change_temperature(self, client, userdata, message):
+        print("command:change temp, value: %s" % message.payload)
         try:
             self.temperatureValue = int(message.payload)
         except:
-            print("can't change temp to %s" % message.payload)
+            pass
 
     def command_change_light(self, client, userdata, message):
-        print("CMD: Change light, value: %s" % message.payload)
+        print("command:change light, value: %s" % message.payload)
         try:
-            self.lightValue = int(message.payload)
+            self.lightValue = bool(message.payload)
         except:
-            print("can't change light to %s" % message.payload)
+            pass
+
 
     def progress_upd(self):
-        if self.progress >= 100:
-            self.progress = 0
-            self.detail_count += 1
-        else:
-            self.progress = self.progress + 25
+
+        match self.course:
+            case "forward_X":
+                self.progress['X'] += 1
+                if self.progress['X'] >= 10:
+                    self.course = "right_Y"
+            case "right_Y":
+                self.progress['Y'] += 1
+                self.course = "backward_X"
+            case "backward_X":
+                self.progress['X'] -= 1
+                if self.progress['X'] <= 0:
+                    self.course = "left_Y"
+            case "left_Y":
+                self.progress['Y'] += 1
+                self.course = "forward_X"
         try:
             f = open('./save_state.txt', 'r')
             file = f.read()
@@ -53,19 +65,13 @@ class Machine():
             config = {}
         f = open('./save_state.txt', 'w')
         config["progress"] = self.progress
-        config["details"] = self.detail_count
+        config["course"] = self.course
         config = json.dumps(config)
         f.write('%s' % config)
         f.close()
-        print("detail count = %d, progress now = %d%% " % (self.detail_count, self.progress))
-#        f = open('./save_state.txt', 'w')
-#        config = json.dumps({"details": self.detail_count, "progress": self.progress})
-#        f.write('%s' % config)
-#        f.close()
 
     def get_data(self):
-        data = json.dumps({"temp": self.temperatureValue, "light": self.lightValue, "detail_count": self.detail_count,
-                           "time": str(datetime.now())})
+        data = json.dumps({"progress": self.progress, "course": self.course, "time": str(datetime.now())})
         return data
 
 
@@ -112,23 +118,21 @@ def main():
     mqtt_client = init("client_id")
     run(mqtt_client)
     # read config
-    details = 0
-    progress = 0
-    argv = sys.argv
+    progress = {'X': 0, 'Y': 0}
+    argv = sys.argv;
     try:
         path_to_config = argv[1] if len(argv) > 1 else "./save_state.txt"
         f = open(path_to_config, 'r')
         file = f.read()
         config = json.loads(file)
         f.close()
-        details = int(config["details"])
-        progress = int(config["progress"])
+        progress = dict(config["progress"])
         print("read config %s" % config)
     except:
         pass
 
     # init machine
-    machine = Machine(mqtt_client, details, progress)
+    machine = Machine(mqtt_client, progress)
 
     while True:
         time.sleep(2)
